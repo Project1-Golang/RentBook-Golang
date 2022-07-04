8858393044fe1c23bd0ff4fba782bc9586ffdc1d
Yosef Budi Yudo Prawiro <yudop27@gmail.com>
2022-07-04 14:52:09 +0700

    Update ERD

---
+++ config/mysql-driver.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+func ConnectionDB() *sql.DB {
+	dbConnection := os.Getenv("DB_CONNECTION")
+	db, err := sql.Open("mysql", dbConnection)
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println("success")
+	}
+	return db
+}
