8cbbd135a86ea3c9095876c78ea7cbbd2a0b0a12
Yosef Budi Yudo Prawiro <yudop27@gmail.com>
2022-07-04 15:08:47 +0700

    Update ALL

--- Entity/Users.go
+++ Entity/Users.go
@@ -4,12 +4,12 @@ import "time"
 
 type users struct {
 	id_user    string `gorm:"primaryKey;type:varchar(36);"`
-	name       string
+	name       string `json:"name" binding:"required"`
 	status     bool
 	nomer_HP   string
-	email      string
-	user_Name  string
-	password   string
+	email      string `gorm:"unique" json:"email" binding:"required,email"`
+	user_Name  string `gorm:"unique" json:"username" binding:"required"`
+	password   string `json:"password"`
 	address    string
 	created_at time.Time `gorm:"autoCreateTime"`
 	updated_at time.Time `gorm:"autoCreateTime"`
--- go.mod
+++ go.mod
@@ -1,9 +1,3 @@
 module proyek1
 
 go 1.18
-
-require (
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	gorm.io/gorm v1.23.7 // indirect
-)
