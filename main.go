b96a1c8a370c5fb173983b86e7bfd49e11652a9c
reski-id <pesan.reski@gmail.com>
2022-07-05 15:58:08 +0700

    select id,title,author book

--- main.go
+++ main.go
@@ -25,26 +25,22 @@ func main() {
 
 		switch input {
 		case 1:
-			// var newUsers entity.AksesUsers
+			var newUsers entity.AksesUsers
 			// newUsers.Id_user = 1
-			// fmt.Print("Masukkan nama: ")
-			// fmt.Scanln(&newUsers.name)
-			// fmt.Print("Masukkan nomorhp: ")
-			// fmt.Scanln(&newUsers.nomer_HP)
-			// fmt.Print("Masukkan email: ")
-			// fmt.Scanln(&newUsers.email)
-			// fmt.Print("Masukkan user_Name: ")
-			// fmt.Scanln(&newUsers.user_name)
-			// fmt.Print("Masukkan password: ")
-			// fmt.Scanln(&newUsers.password)
-			// fmt.Print("Masukkan address: ")
-			// fmt.Scanln(&newUsers.address)
-			// res := AksesUsers.TambahUserBaru(newUsers)
-			// if res.ID == 0 {
-			// 	fmt.Println("Tidak bisa input data, ada error")
-			// 	break
-			// }
-			fmt.Println("Berhasl input data")
+			fmt.Print("Masukkan nama: ")
+			fmt.Scanln(&newUsers)
+			fmt.Print("Masukkan nomorhp: ")
+			fmt.Scanln(&newUsers)
+			fmt.Print("Masukkan email: ")
+			fmt.Scanln(&newUsers)
+			fmt.Print("Masukkan user_Name: ")
+			fmt.Scanln(&newUsers)
+			fmt.Print("Masukkan password: ")
+			fmt.Scanln(&newUsers)
+			fmt.Print("Masukkan address: ")
+			fmt.Scanln(&newUsers)
+
+			fmt.Println(newUsers)
 		case 2:
 			fmt.Println("Menu Log In")
 			fmt.Print("Masukkan user_Name: ")
@@ -55,7 +51,7 @@ func main() {
 		case 3:
 			fmt.Println("Daftar Buku Yang Ada")
 			for _, val := range AksesBook.GetAllData() {
-				fmt.Println(val)
+				fmt.Println(val.Id_book, val.Title_book, val.Author)
 			}
 		default:
 			continue
