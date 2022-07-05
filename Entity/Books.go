bc4832ded61023af1b31b94b032697ca18360398
Yosef Budi Yudo Prawiro <yudop27@gmail.com>
2022-07-05 10:57:41 +0700

    Is not in GOROOT Error

--- Entity/Books.go
+++ Entity/Books.go
@@ -1,6 +1,11 @@
 package entity
 
-import "time"
+import (
+	"log"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type books struct {
 	id_book     string  `gorm:"primaryKey;type:varchar(36);"`
@@ -13,3 +18,64 @@ type books struct {
 	updated_at  time.Time `gorm:"autoCreateTime"`
 	deleted_at  time.Time `gorm:"index"`
 }
+
+type AksesBook struct {
+	DB *gorm.DB
+}
+
+func (as *AksesBook) GetAllData() []books {
+	var daftarBook = []books{}
+	// err := as.DB.Raw("Select * from books").Scan(&daftarBook)
+	err := as.DB.Find(&daftarBook)
+	if err.Error != nil {
+		log.Fatal(err.Statement.SQL.String())
+		return nil
+	}
+
+	return daftarBook
+}
+
+// func (as *AksesBook) TambahBukuBaru(newBook books) books {
+// 	if newBook.name == "Harry Potter" {
+// 		newBook.id = uint(1)
+// 	}
+// 	uid := uuid.New()
+// 	books.id = uid.String()
+// 	err := as.DB.Create(&books).Error
+// 	if err != nil {
+// 		log.Println(err)
+// 		return books{}
+// 	}
+
+// 	return newBook
+// }
+
+// func (as *AksesBook) GetSpecificBuku(UID int) books {
+// 	var daftarBook = books{}
+// 	daftarBook.id_book = uint(UID)
+// 	// err := as.DB.Raw("Select * from student").Scan(&daftarStudent)
+// 	err := as.DB.First(&daftarBook)
+// 	if err.Error != nil {
+// 		log.Fatal(err.Statement.SQL.String())
+// 		return books{}
+// 	}
+
+// 	return daftarBook
+// }
+
+// func (as *AksesBook) HapusBuku(IDBook int) bool {
+// 	postExc := as.DB.Where("ID = ?", id_book).Delete(&books{})
+// 	// ada masalah ga(?)
+// 	if err := postExc.Error; err != nil {
+// 		log.Fatal(err)
+// 		return false
+// 	}
+// 	// berapa data yang berubah (?)
+// 	if aff := postExc.RowsAffected; aff < 1 {
+// 		log.Println("Tidak ada data yang dihapus")
+// 		return false
+// 	}
+
+// 	return true
+
+// }
--- Entity/Users.go
+++ Entity/Users.go
@@ -1,6 +1,11 @@
 package entity
 
-import "time"
+import (
+	"log"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type users struct {
 	id_user    string `gorm:"primaryKey;type:varchar(36);"`
@@ -15,3 +20,64 @@ type users struct {
 	updated_at time.Time `gorm:"autoCreateTime"`
 	deleted_at time.Time `gorm:"index"`
 }
+
+type AksesUsers struct {
+	DB *gorm.DB
+}
+
+func (as *AksesUsers) GetAllData() []users {
+	var daftarUsers = []users{}
+	// err := as.DB.Raw("Select * from student").Scan(&daftarStudent)
+	err := as.DB.Find(&daftarUsers)
+	if err.Error != nil {
+		log.Fatal(err.Statement.SQL.String())
+		return nil
+	}
+
+	return daftarUsers
+}
+
+// func (as *AksesUsers) TambahUserBaru(newUsers users) users {
+// 	if newUsers.name == "Jerry" {
+// 		newUsers.id_user = uint(1)
+// 	}
+// 	uid := uuid.New()
+// 	newUsers.id_user = uid.String()
+// 	err := as.DB.Create(&newUsers).Error
+// 	if err != nil {
+// 		log.Println(err)
+// 		return users{}
+// 	}
+
+// 	return newUsers
+// }
+
+// func (as *AksesUsers) GetSpecificUser(UID int) users {
+// 	var daftarUsers = users{}
+// 	daftarUsers.id_user = uint(UID)
+// 	// err := as.DB.Raw("Select * from student").Scan(&daftarStudent)
+// 	err := as.DB.First(&daftarUsers)
+// 	if err.Error != nil {
+// 		log.Fatal(err.Statement.SQL.String())
+// 		return users{}
+// 	}
+
+// 	return daftarUsers
+// }
+
+// func (as *AksesUsers) HapusMurid(IDUsers int) bool {
+// 	postExc := as.DB.Where("ID = ?", IDUsers).Delete(&users{})
+// 	// ada masalah ga(?)
+// 	if err := postExc.Error; err != nil {
+// 		log.Fatal(err)
+// 		return false
+// 	}
+// 	// berapa data yang berubah (?)
+// 	if aff := postExc.RowsAffected; aff < 1 {
+// 		log.Println("Tidak ada data yang dihapus")
+// 		return false
+// 	}
+
+// 	return true
+
+// }
